7bff431a1a6728ec182d6df46f40233b0e80ff24
Bogdan Gabor <bogdan.gabor.cj@gmail.com>
2025-05-11 22:22:22 +0300

    Add alwayscenter and swallow patches

--- config.h
+++ config.h
@@ -3,6 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
@@ -28,9 +29,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-    {  "none",    NULL,       NULL,       0,            0            -1},
-	// { "Gimp",     NULL,       NULL,       0,            1,           -1 },
+
+	/* class       instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "ghostty",   NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,        NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
@@ -66,6 +68,7 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|Mod1Mask,              XK_t,      spawn,          SHCMD("if tmux ls &> /dev/null; then ghostty -e tmux -u attach; else ghostty -e tmux -u new-session -s system; fi") },
 
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
